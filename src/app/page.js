9f50a5dadb66668a7aa66b41a021ba4007122b53
Oscar Perez <55750435+oscar2901-UNIVA@users.noreply.github.com>
2025-03-04 14:25:59 +0100

    feat: add confetti and change image of the app

--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,16 @@ import Map from "./components/Map";
 import Gallery from "./components/Gallery";
 import LoveLetters from "./components/LoveLetters";
 import TypewriterEffect from "./components/Typewriter";
+import FloatingHearts from "./components/FloatingHearts";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white p-6">
+      <FloatingHearts />
       <Counter />
       <Playlist />
       <Timeline />
-      <Map />
+      {/* <Map /> */}
       <Gallery />
       <LoveLetters />
       <TypewriterEffect />
